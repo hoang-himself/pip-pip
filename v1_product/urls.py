20eaa94c1741b17b15247b608acb61a709189ad4
simplynormal <nguyenchinhkhoi@gmail.com>
2021-12-07 10:42:00 +0000

    RC product

--- v1_product/urls.py
+++ v1_product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductView
+from .views import (AllProductView, ProductView)
 
 app_name = 'v1_product'
 
 urlpatterns = [
+    path('all', AllProductView.as_view()),
     path('product', ProductView.as_view(), name='product_mgmt'),
 ]
--- v1_product/views.py
+++ v1_product/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 
 from master_api.utils import get_by_uuid
 from master_api.views import (
-    create_object, edit_object, delete_object, get_object
+    create_object, edit_object, delete_object, get_object, get_all_object
 )
 from master_db.models import Product
 from master_db.serializers import ProductSerializer
@@ -13,15 +13,20 @@ from master_db.serializers import ProductSerializer
 CustomUser = get_user_model()
 
 
+class AllProductView(APIView):
+    def get(self, _):
+        return get_all_object(Product)
+
+
 class ProductView(APIView):
-    def post(self, request):
-        return create_object(Product, data=request.data)
+    # def post(self, request):
+    #     return create_object(Product, data=request.data)
 
     def get(self, request):
-        return get_object(Product, data=request.GET)
+        return get_object(Product, data=request.data)
 
-    def patch(self, request):
-        return edit_object(Product, data=request.data)
+    # def patch(self, request):
+    #     return edit_object(Product, data=request.data)
 
-    def delete(self, request):
-        return delete_object(Product, data=request.data)
+    # def delete(self, request):
+    #     return delete_object(Product, data=request.data)
