f47b236c90151bfbb3ca665e7506a392a50fcc1c
simplynormal <nguyenchinhkhoi@gmail.com>
2021-12-07 10:30:22 +0000

    RC brand

--- v1_brand/urls.py
+++ v1_brand/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BrandView
+from .views import (AllBrandView, BrandView)
 
 app_name = 'v1_brand'
 
 urlpatterns = [
+    path('all', AllBrandView.as_view()),
     path('brand', BrandView.as_view(), name='brand_mgmt'),
 ]
--- v1_brand/views.py
+++ v1_brand/views.py
@@ -8,19 +8,24 @@ from master_db.models import Brand
 # from master_db.serializers import BrandSerializer
 # from master_api.utils import get_list_or_404
 from master_api.views import (
-    create_object, edit_object, delete_object, get_object
+    create_object, edit_object, delete_object, get_object, get_all_object
 )
 
 
+class AllBrandView(APIView):
+    def get(self, _):
+        return get_all_object(Brand)
+
+
 class BrandView(APIView):
-    def post(self, request):
-        return create_object(Brand, data=request.data)
+    # def post(self, request):
+    #     return create_object(Brand, data=request.data)
 
     def get(self, request):
-        return get_object(Brand, data=request.GET)
+        return get_object(Brand, data=request.data)
 
-    def delete(self, request):
-        return delete_object(Brand, data=request.data)
+    # def delete(self, request):
+    #     return delete_object(Brand, data=request.data)
 
-    def patch(self, request):
-        return edit_object(Brand, data=request.data)
+    # def patch(self, request):
+    #     return edit_object(Brand, data=request.data)
